c0a611a9a52dd40db6842b9af22dfb7cf331b407
radheysm <radheysaraan@gmail.com>
2020-05-17 10:39:53 +0530

    Last commit was error because a number is divisible by 4 and 100 and 400 it should be a leap year

--- leap.cpp
+++ leap.cpp
@@ -6,9 +6,12 @@ int leap_year(int n){
         return -1;
     }
     if(n%4==0){
-        if(n%4==0){
-            return 0;
+        if(n%100==0){
+           if(n%400==0){
+               return 1;
+           }
         }
+        return 1;
     }
     else{
         return 0;
