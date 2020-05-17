293c1e3646b3a0becebecd1896e4ab98ef78d5c1
radheysm <radheysaraan@gmail.com>
2020-05-17 10:31:26 +0530

    Started with base case if user Insert a negative number it should give output that Not a valid Input

---
+++ leap.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int leap_year(int n){
+    
+    if(n<0){
+        return -1;
+    }
+
+    if(n%4==0){
+        return 1;
+    }
+
+     return 0;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    if(leap_year(n) == 1) {
+        cout<<"It's a Leap Year";
+    }
+    else if(leap_year(n)==(-1)){
+        cout<<"Not a valid Input";
+    }
+    else{
+        cout<<"It's Not a Leap Year";
+    }
+}
